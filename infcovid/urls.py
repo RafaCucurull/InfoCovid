a92c81f8ef52df25cf30d2cd0a334713b0416b20
Gerard Guerrero Lacasa <gerard@MacBookdeGerard.lan>
2021-03-07 18:52:05 +0100

    devpagesGerard

--- infcovid/urls.py
+++ infcovid/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
+    path('/consultes', views.consultes, name='consultes'),
 ]
--- infcovid/views.py
+++ infcovid/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'home.html')
+
+def consultes(request):
+    return render(request, 'consultes.html')
