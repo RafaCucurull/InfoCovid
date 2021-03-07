338eac10f5d1ba976e26d544b450d0cae4bc937f
Gerard Guerrero Lacasa <gerard@MacBookdeGerard.lan>
2021-03-07 18:12:31 +0100

    homepage

--- ProjecteModelsProces/urls.py
+++ ProjecteModelsProces/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('infcovid.urls', namespace='infocovid')),
+    path('', include('infcovid.urls')),
 ]
--- infcovid/urls.py
+++ infcovid/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', include('infcovid.urls', namespace='infocovid')),
+    path('', views.homepage, name='home'),
 ]
