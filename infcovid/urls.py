70cbe386d24abf41cb1b202b3a0633eae183a921
Gerard Guerrero Lacasa <gerard@MacBookdeGerard.lan>
2021-03-12 13:20:02 +0100

    register

--- infcovid/urls.py
+++ infcovid/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
-    path('/consultes', views.consultes, name='consultes'),
+    path('consultes/', views.consultes, name='consultes'),
 ]
