7f4aa2b89362973bf19dd4d813726862bdc2c0d9
rafa <rafacucugaz9911@gmail.com>
2021-03-24 10:27:24 +0100

    infomalaltia

--- infcovid/urls.py
+++ infcovid/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
+    path('mascaretes/', views.infomascaretes, name="mascaretes"),
+    path('informacio/', views.infogeneral, name="infogeneral")
 ]
--- infcovid/views.py
+++ infcovid/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'home.html')
+
+def infomascaretes(request):
+    return render(request, 'mascaretes.html')
+
+def infogeneral(request):
+    return render(request, 'informacio_general.html')
