4b3c7d2e80c2071dd462b65681021993c5f19156
Gerard Guerrero Lacasa <gerard@MacBookdeGerard.lan>
2021-03-24 11:29:46 +0100

    register fix

--- users/views.py
+++ users/views.py
@@ -6,10 +6,7 @@ def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save()
             login(request, user)
             return redirect('home')
     else:
